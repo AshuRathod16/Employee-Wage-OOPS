be5f2e2396f5408ee04144481df89aaf8746c6ea
Ashwini Rathod <rashwini510@gmail.com>
2022-06-18 21:37:14 +0530

    Added: Check the employees attendance

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -3,7 +3,16 @@ package com.bridgelabz;
 public class EmpWage {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to employee wage program");
+        int isFULL_TIME = 1;
+        double empcheck = (int) (Math.random() * 100) % 2;
+        if (empcheck == isFULL_TIME) {
+
+            System.out.println("Employee is Present");
+        }
+        else
+
+            System.out.println("Employee is Absent");
+
     }
 }
 
