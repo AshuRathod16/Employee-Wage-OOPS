299dee2402653fd58e75a32e8e6abb52a1199439
Ashwini Rathod <rashwini510@gmail.com>
2022-06-18 23:22:06 +0530

    Added: Employee wage for multiple companies

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -3,53 +3,54 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmpWage {
-    static final int WAGE_PER_HR = 20;
-    static final int FULL_DAY_HR = 8;
-    static final int PART_TIME_HR = 4;
-    static final int MAX_WORKING_DAYS = 20;
-    static final int MAX_HRS_PER_MONTH = 100;
-    static final int IS_FULL_TIME = 0;
-    static final int IS_PART_TIME = 1;
-
-    public void employeeWage() {
-        System.out.println("Welcome to Employee Wage Program");
-        Random random = new Random();
-        int dailyEmpWage;
-        int totalWage = 0;
-        int totalWorkingHrs = 0;
-        int totalWorkingDays = 0;
-        int empHours = 0;
-
-        while (totalWorkingHrs < MAX_HRS_PER_MONTH && totalWorkingDays < MAX_WORKING_DAYS) {
-            totalWorkingDays++;
-            double empCheck = (Math.floor(Math.random() * 10) % 3);
-            switch ((int) empCheck) {
-                case IS_FULL_TIME:
-                    System.out.println("Employee present");
-                    empHours = FULL_DAY_HR;
-                    break;
-                case IS_PART_TIME:
-                    System.out.println("Employee present for part time");
-                    empHours = PART_TIME_HR;
-                    break;
-                default:
-                    System.out.println("Employee Absent ");
-                    empHours = 0;
-            }
-            totalWorkingHrs += empHours;
-        }
-        totalWage = totalWorkingHrs * WAGE_PER_HR;
-        System.out.println("Total Wage: " + totalWage);
-        System.out.println("total working hours: " + totalWorkingHrs);
-        System.out.println("total working Days: " + totalWorkingDays);
-    }
+    public final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    private int EMP_RATE_PER_HOURS;
+    private int NUM_OF_WORKING_DAYS;
+    private int MAX_HRS_IN_MONTH;
 
     public static void main(String[] args) {
-        //Creating object
+        System.out.println("Welcome to employee wage using OOPs concept");
         EmpWage empWage = new EmpWage();
-        // calling the method
-        empWage.employeeWage();
+        empWage.computeWage("AMAZON",20,20,100);
+        empWage.computeWage("FLIPCART",10,15,100);
     }
+
+
+    public void computeWage(String company,int empRatePerHour,int numberOfWorkingDays,int maxHrs) {
+        this.EMP_RATE_PER_HOURS = empRatePerHour;
+        this.NUM_OF_WORKING_DAYS = numberOfWorkingDays;
+        this.MAX_HRS_IN_MONTH = maxHrs;
+        int empHours;
+        int empwage = 0;
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
+        int totalEmpwage = 0;
+
+
+
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            int empCheck = (int)(Math.floor(Math.random() * 10) % 3);
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+                default:
+                    empHours = 0;
+            }
+            totalEmpHrs += empHours;
+            empwage = empHours * EMP_RATE_PER_HOURS;
+            System.out.println("Day" + totalWorkingDays + "Emp Hrs: " + empHours + "Emp Wage: " + empwage);
+            totalEmpwage += empwage;
+        }
+        totalEmpwage = totalEmpHrs * EMP_RATE_PER_HOURS;
+        System.out.println("Total Emp Wage of an employee in " + company + "is" + totalEmpwage);
+
     }
+}
 
 
