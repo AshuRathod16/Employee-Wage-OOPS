d2ef3ef4794b9b955d6108ac451a193a9850ab42
Ashwini Rathod <rashwini510@gmail.com>
2022-06-18 23:46:06 +0530

    Added: Empwage of multiple companies using interface approach

--- src/com/bridgelabz/CompanyEmpWage.java
+++ src/com/bridgelabz/CompanyEmpWage.java
@@ -1,6 +1,11 @@
 package com.bridgelabz;
+interface IEmpWage{
+    public void addCompanyEmpWage(String companyName, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth);
 
+    public void computeEmpWage();
+}
 public class CompanyEmpWage {
+
     private String companyName;
     private int empRatePerHr;
     private int numOfWorkingDays;
